c47f8b3a99dbfd30b7195a278fd021875938de1e
Sahad Mahaboob <sahadmahaboob@gmail.com>
2023-10-21 01:18:20 +0530

    added api configuration in environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: '', // Example API base URL for production
+  apiUrl: 'https://gorest.co.in/public/v2/users'
+};
---
+++ src/environments/environments.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: 'http://localhost:4200', // Example API base URL for development
+    apiUrl: 'https://gorest.co.in/public/v2/users'
+  };
